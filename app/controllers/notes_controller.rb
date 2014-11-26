57494826b65ef02256e148b20ac3e2b43bdc1806
App Academy Student <student@appacademy.io>
2014-11-25 17:26:16 -0800

    added notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,41 @@
-class NotesController < ApplicationController
+module Api
+  class NotesController < ApiController
+    #before_action #require_notebook_member!
+
+    def create
+      @note = current_notebook.notes.new(note_params)
+
+      if @note.save
+        render json: @note
+      else
+        render json: @note.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @note = Note.find(params[:id])
+      if @note.update(note_params)
+        render json: @note
+      else
+        render json: @note.erorrs.full_messages, status: :unprocessable_entity
+      end
+    end
+
+
+    private
+
+    def current_notebook
+      if params[:id]
+        @note = Note.find(params[:id])
+        @notebook = @note.notebook
+      elsif params[:note]
+        @notebook = Notebook.find(params[:note][:notebook_id])
+      end
+    end
+
+    def note_params
+      params.require(:note).permit(:title, :body, :notebook_id)
+    end
+
+  end
 end
