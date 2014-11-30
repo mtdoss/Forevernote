7fd0758f6fbc48c2b7091c68a75ea0e6bd4d1c72
Mark <mtdoss@gmail.com>
2014-11-30 13:07:02 -0800

    ready to add form page i think

--- app/assets/javascripts/views/notebooks/index_item.js
+++ app/assets/javascripts/views/notebooks/index_item.js
@@ -7,7 +7,10 @@ Evernote.Views.NotebooksIndexItem = Backbone.View.extend({
   },
 
   events: {
-    "click": "addNotebookShow"
+    "click": function(event) {
+      this.addNotebookShow(event);
+      this.classChange(event);
+    }
   },
 
   render: function() {
@@ -31,5 +34,14 @@ Evernote.Views.NotebooksIndexItem = Backbone.View.extend({
     // this._currentView = notebookShow
     // this.$el.find('.notebook-show').html(notebookShow.render().$el)
     this.parent.addSubviewReplacement('.notebook-show', notebookShow);
+  },
+
+  classChange: function(event) {
+    if (this.parent._clickedOn) {
+      this.parent._clickedOn.css({background: '#eaedef' });
+    }
+
+    this.parent._clickedOn = $(event.currentTarget);
+    this.parent._clickedOn.css({background: '#ADADB1'});
   }
 })
