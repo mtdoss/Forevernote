4ee3ac2e51ccf08e8ac86ef38962f582fb861f32
Mark <mtdoss@gmail.com>
2014-12-02 16:35:24 -0800

    Created tag index item view, seems like it should work but it isn't

--- app/assets/javascripts/views/tags/index.js
+++ app/assets/javascripts/views/tags/index.js
@@ -1,8 +1,19 @@
-Evernote.Views.TagsIndex = Backbone.View.extend({
+Evernote.Views.TagsIndex = Backbone.CompositeView.extend({
   template: JST["tags/index"],
 
   initialize: function(options){
-    this.tags = options.tags
+    this.tags = options.tags;
+    debugger;
+    // this.listenTo(this.tags, )
+    this.tags.each(this.addIndexItem.bind(this));
+  },
+
+  addIndexItem: function(indexItem){
+    var indexItemView = new Evernote.Views.TagIndexItem({
+      model: indexItem
+    });
+
+    this.addSubview('tags-index', indexItemView);
   },
 
   render: function() {
---
+++ app/assets/javascripts/views/tags/index_item.js
@@ -0,0 +1,12 @@
+Evernote.Views.TagIndexItem = Backbone.View.extend({
+  template: JST["tags/index_item"],
+
+  render: function(){
+    var content = this.template({
+      tag: this.model
+    });
+
+    this.$el.html(content);
+    return this;
+  }
+});
