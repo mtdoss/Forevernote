6a01dcf6f00fe62db5c35ecafc577c081a4283f1
Mark <mtdoss@gmail.com>
2014-12-03 13:26:33 -0800

    minor fix to delete button

--- app/assets/javascripts/collections/notes.js
+++ app/assets/javascripts/collections/notes.js
@@ -1,6 +1,7 @@
 Evernote.Collections.Notes = Backbone.Collection.extend({
   url: "/api/notes",
   model: Evernote.Models.Note,
+  comparator: "created_at",
 
   initialize: function(models, options) {
     this.notebook = options.notebook;
