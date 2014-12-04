2664ac3933c87bec57046bce66544524ec732818
Mark <mtdoss@gmail.com>
2014-12-04 10:25:02 -0800

    Beginning to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+guest_user = User.create(email: "guest@forevernote.com", password: "star_wars")
+guest_user.
