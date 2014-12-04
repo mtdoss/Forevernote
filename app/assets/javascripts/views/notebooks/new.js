32b3c0759aa8da9c7f33cf70d9032f067202cc00
Mark <mtdoss@gmail.com>
2014-12-03 16:32:44 -0800

    fixed a typo that made title not save

--- app/assets/javascripts/views/notebooks/new.js
+++ app/assets/javascripts/views/notebooks/new.js
@@ -17,7 +17,8 @@ Evernote.Views.NotebooksNew = Backbone.View.extend({
     var view = this;
     var params = $(event.currentTarget).serializeJSON();
     debugger;
-    this.model.set(params);
+    this.model.set("title", $("#notebook-title").val());
+    // this.model.set(params);
     if (this.model.isNew()) {
       Evernote.Collections.notebooks.create(this.model, {});
     } else {
